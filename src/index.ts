be8f288b252cdbfa3b9be9460c14cecdcbb94608
Travis Fischer <fisch0920@gmail.com>
2024-09-30 03:25:09 -0400

    ✋

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './linkedin-client'
+export * from './linkedin-utils'
 export * from './types'
 export * from './utils'
--- src/utils.ts
+++ src/utils.ts
@@ -23,29 +23,6 @@ export function encodeCookies(cookies: Record<string, SetCookie>): string {
 }
 
 /**
- * Return the ID of a given Linkedin URN.
- *
- * Example: urn:li:fs_miniProfile:<id>
- */
-export function getIdFromUrn(urn?: string) {
-  return urn?.split(':').at(-1)
-}
-
-/**
- * Return the URN of a raw group update
- *
- * Example: urn:li:fs_miniProfile:<id>
- * Example: urn:li:fs_updateV2:(<urn>,GROUP_FEED,EMPTY,DEFAULT,false)
- */
-export function getUrnFromRawUpdate(update?: string) {
-  return update?.split('(')[1]?.split(',').at(0)?.trim()
-}
-
-export function isLinkedInUrn(urn?: string) {
-  return urn?.startsWith('urn:li:') && urn.split(':').length >= 4
-}
-
-/**
  * From `inputObj`, create a new object that does not include `keys`.
  *
  * @example
